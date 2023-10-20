458935aff579ee09f47e1fcc8162c77593dd37e7
Firmino Veras <firmino.veras@gmail.com>
2023-10-20 14:57:13 -0300

    init dot files

---
+++ eww/scripts/set-focus-mode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hyprctl keyword general:border_size 0
+hyprctl keyword decoration:rounding 0
---
+++ rofi/rofi_launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rofi \
+    -show drun \
+    -theme /home/firmino/.config/waybar/style/rofi/rofi.rasi
